19de8009e5085a0c2d3a4a7eb62ba7b3b2069d63
Shamir Pontes <Shamir Pontes@PAT-0315>
2022-04-18 18:58:36 -0300

    Add project files.

---
+++ CpmPedidos.Repository/Maps/IBaseDomainMap.cs
@@ -0,0 +1,10 @@
+﻿using CpmPedidos.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CpmPedidos.Repository.Maps
+{
+    public interface IBaseDomainMap<TDomain> where TDomain : BaseDomain
+    {
+        void Configure(EntityTypeBuilder<TDomain> builder);
+    }
+}
